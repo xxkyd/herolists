7c75d3d46addf99a735dd60faf76b9d50229b2e1
kuyedie <1055188318@qq.com>
2019-07-10 17:53:49 +0800

    配置项目下载文件创建路由

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "./assets/css/index.css";
+import axios from 'axios'
+Vue.prototype.axios = axios
+import router from './router'
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
